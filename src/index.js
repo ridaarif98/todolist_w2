d09f60ad5810637118db1c07f600b19ba37833b6
ridaarif98 <ridaarif16@gmail.com>
2021-07-26 11:34:20 +0200

    Store todo list data in local storage and display it on load

--- src/index.js
+++ src/index.js
@@ -4,6 +4,21 @@ import './style.css';
 const form = document.getElementById('addTodo');
 let collection= [];
 
+function ShowList(arr) {
+  const listBook = arr.map((b) => `
+  <ul class="testList1" draggable="true">
+    <ul class="testList">
+        <li><input type="checkbox" value='${b.complete}' class="checkboX"></li>
+        <li>${b.title}</li>
+    </ul>
+    <ul class="iconList">
+        <li><i class="fa fa-ellipsis-v" id='${b.id}'></i></li>
+    </ul>           
+    </ul>
+   `).join('');
+  document.getElementById('showListItem').innerHTML = `${listBook}`;  
+}
+
 function addList() {
   const todo = {
     id: collection.length,
@@ -18,6 +33,17 @@ function addList() {
   form.reset();
 }
 
+window.addEventListener('load', () => {
+  const dataGet = localStorage.getItem('todoObject');
+  const data = JSON.parse(dataGet);
+  if (data) {
+    collection = data;
+  }
+  if (collection.length > 0) {
+    ShowList(collection);
+  }
+});
+  
 form.addEventListener('submit', (ev) => {
     ev.preventDefault();
     addList();
