4dfbb5335b3344374a5833af279744521209f679
ridaarif98 <ridaarif16@gmail.com>
2021-07-26 11:30:55 +0200

    For storing todo list add script

--- src/index.js
+++ src/index.js
@@ -1,2 +1,25 @@
 import _ from 'lodash';
 import './style.css'; 
+
+const form = document.getElementById('addTodo');
+let collection= [];
+
+function addList() {
+  const todo = {
+    id: collection.length,
+    title: document.getElementById('todotitle').value,
+    complete: false,
+  };
+  collection.push(todo);
+  localStorage.setItem('todoObject', JSON.stringify(collection));
+  if (collection.length > 0) {
+    ShowList(collection);
+  }
+  form.reset();
+}
+
+form.addEventListener('submit', (ev) => {
+    ev.preventDefault();
+    addList();
+});
+  
